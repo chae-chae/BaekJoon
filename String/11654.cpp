7c27a1f9681981828d86c9305546d4cb1cebd9cd
chae-chae <54352086+chae-chae@users.noreply.github.com>
2020-01-03 23:28:27 +0900

    Create 11654.cpp

---
+++ String/11654.cpp
@@ -0,0 +1,15 @@
+//
+//  11654.cpp
+//  BaekJoon
+//
+//  Created by 채채 on 03/01/2020.
+//  Copyright © 2020 chaechae. All rights reserved.
+//
+
+#include <stdio.h>
+int main(void){
+    char input;
+    scanf("%c", &input);
+    printf("%d\n", input);
+    return 0;
+}
