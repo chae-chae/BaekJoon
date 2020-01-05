acccb932fefc219c7fdb747fe25c7118acf16bf4
chae-chae <54352086+chae-chae@users.noreply.github.com>
2020-01-06 00:14:57 +0900

    Add files via upload

---
+++ 2441.cpp
@@ -0,0 +1,23 @@
+//
+//  2441.cpp
+//  BaekJoon
+//
+//  Created by 채채 on 05/01/2020.
+//  Copyright © 2020 chaechae. All rights reserved.
+//
+
+#include <stdio.h>
+int main(void){
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i<n; i++) {
+        if (i > 0) {
+            printf("%*c", i, ' ');
+        }
+        for (int j = n-i; j>0; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ 2558.cpp
@@ -0,0 +1,16 @@
+//
+//  2558.cpp
+//  BaekJoon
+//
+//  Created by 채채 on 05/01/2020.
+//  Copyright © 2020 chaechae. All rights reserved.
+//
+
+#include <stdio.h>
+int main(void){
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", a+b);
+    return 0;
+}
