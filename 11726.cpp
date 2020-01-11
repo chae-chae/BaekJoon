def1d5b2be7205f1a9bc251d8b5e4cef47139df9
chae-chae <54352086+chae-chae@users.noreply.github.com>
2020-01-11 23:21:00 +0900

    Create 11726.cpp

---
+++ 11726.cpp
@@ -0,0 +1,28 @@
+//
+//  11726.cpp
+//  BaekJoon
+//
+//  Created by 채채 on 11/01/2020.
+//  Copyright © 2020 chaechae. All rights reserved.
+//
+
+
+//  dp랑 memoization 연습하기!
+
+#include <stdio.h>
+
+int d[1001];
+int calc(int n);
+int main(void){
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", calc(n));
+    return 0;
+}
+
+int calc(int n){
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    if (d[n] != 0) return d[n];
+    return d[n] = (calc(n-1) + calc(n-2)) % 10007;
+}
