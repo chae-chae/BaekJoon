1e8213351ee12581187648aa06239024124daf51
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-03-08 19:39:43 +0900

    Add files via upload

---
+++ 11382.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+const ans = input.reduce((a, b) => a + b);
+console.log(ans);
