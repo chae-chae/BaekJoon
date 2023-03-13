2892b7cd97c06887117711f34c645d6d7cea8762
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-03-13 23:33:49 +0900

    Create 1330.js

---
+++ 1330.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+const A = input[0];
+const B = input[1];
+
+if(A>B){
+    console.log(">");
+} else if(A<B){
+    console.log("<");
+} else {
+    console.log("==");
+}
