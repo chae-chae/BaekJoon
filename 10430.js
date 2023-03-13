88f7763122802c2998737f34647d60f4550f1cfb
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-03-13 19:45:04 +0900

    Create 10430.js

---
+++ 10430.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+
+let A = input[0];
+let B = input[1];
+let C = input[2];
+
+console.log((A+B)%C);
+console.log(((A%C) + (B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C) * (B%C))%C);
