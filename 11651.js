5e7665122d92ca70322e9f6fb0cd3f9a5b90f724
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-04-20 18:18:28 +0900

    Create 11651.js

---
+++ 11651.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const n = parseInt(input[0]);
+input.splice(0,1);
+
+let arr = input.map(i => i.split(" ").map(Number));
+
+arr.sort((a, b) => {
+    if(a[1] > b[1]) {return 1}
+    else if(a[1] < b[1]) {return -1}
+    else {
+        if(a[0] > b[0]) {return 1}
+        else if(a[0] < b[0]) {return -1}
+        else {return 0}
+    }
+})
+
+let ans = "";
+arr.map((i) => ans+= `${i[0]} ${i[1]}\n`);
+console.log(ans.trim());
