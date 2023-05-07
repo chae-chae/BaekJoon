6a6fce8f4e5bfab5b96c043baa167277be59a036
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-05-07 18:36:24 +0900

    Create 15439.js

---
+++ 15439.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+
+const n = input[0];
+
+let ans = n*(n-1);
+console.log(ans);
