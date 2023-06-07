28146e2b7339b78ac3c4ef08d88a92864fca880b
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-06-07 15:57:49 +0900

    Create 10699.js

---
+++ 10699.js
@@ -0,0 +1,4 @@
+const date = new Date();
+let ans = `${date.getFullYear()}-${date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}-${date.getDate() < 10 ? `0${date.getDate()}`: date.getDate()}`;
+
+console.log(ans);
