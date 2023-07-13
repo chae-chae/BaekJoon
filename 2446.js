8a25f33d48308317edc24b2d764c26e5de2f90d6
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-07-14 08:53:53 +0900

    Create 2446.js

---
+++ 2446.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n").map(Number);
+
+const ans = [];
+const n = input[0];
+
+let j = 1;
+for(let i = 0; i<n; i++){
+    let str = "";
+    str += " ".repeat(i);
+    str += "*".repeat(2*n-j);
+    j += 2;
+    ans.push(str);
+}
+j = n-2;
+for(let i = 2; i<=n; i++){
+    let str = "";
+    str += " ".repeat(j);
+    j--;
+    str += "*".repeat(2*i-1);
+    ans.push(str);
+}
+
+console.log(ans.join("\n"));
