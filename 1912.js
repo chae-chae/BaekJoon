b80838fdc77bb53dbc54c96e897c425a6b4d2c60
Jeong Chae Kyeong <abed_nadir@naver.com>
2023-07-28 12:45:45 +0900

    Update 1912.js

--- 1912.js
+++ 1912.js
@@ -6,11 +6,27 @@ const n = parseInt(input[0]);
 input.splice(0, 1);
 input = input[0].split(" ").map(Number);
 
-let res = input[0];
+const dp = [input[0]];
 
 for(let i = 1; i<n; i++){
-    input[i] = Math.max(input[i], input[i] + input[i-1]);
-    res = Math.max(input[i], res);
+    dp[i] = Math.max(input[i], dp[i-1] + input[i]);
 }
 
-console.log(res);
+console.log(Math.max(...dp));
+
+// const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+// let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+// const n = parseInt(input[0]);
+// input.splice(0, 1);
+// input = input[0].split(" ").map(Number);
+
+// let res = input[0];
+
+// for(let i = 1; i<n; i++){
+//     input[i] = Math.max(input[i], input[i] + input[i-1]);
+//     res = Math.max(input[i], res);
+// }
+
+// console.log(res);
